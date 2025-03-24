a3079f32829f853bc999c0c4df04db07db1d97d9
partheebanMani <54184647+partheebanMani@users.noreply.github.com>
2025-03-24 23:04:59 +0530

    Revert "new repo for learning javascript along with mocha runner , chai"
    This reverts commit 7cb30e51d6e6fc0e833aab6d3a3c631fbdabb1cc.

--- handlers/customHandlers.cjs
+++
@@ -1,6 +0,0 @@
-const { expect, handler } = require('pactum');
-
-
-handler.addExpectHandler('200success', async(ctx)=>{
-    expect(ctx.res).to.have.status(200, "Failed with invalid response code");
-})

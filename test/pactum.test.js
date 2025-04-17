470c4e31a744c7a3b149511a471732e4bf579ab5
partheebanMani <54184647+partheebanMani@users.noreply.github.com>
2025-04-17 16:59:17 +0530

    added pactum test cases

--- test/pactum.test.js
+++ test/pactum.test.js
@@ -1,8 +1,24 @@
 const { it } = require("mocha");
-const pactum = require("pactum")
+const { spec } = require("pactum")
 
 it("pactum test case", async function () {
 
-    await pactum.spec().get("https://reqres.in/api/users/2").expectStatus(200)
+    await spec().get("https://reqres.in/api/users/2").expectStatus(200)
+
+})
+
+
+//https://reqres.in/api/users?page=2
+
+it("Query param test cases", async function () {
+    const res = await spec()
+        .get("https://reqres.in/api/users")
+        .withQueryParams('page', 2)
+        .expectStatus(200)
+        .toss()
+
+
+
+    console.log(`Response status: ${res.statusCode} and body: ${JSON.stringify(res.body)}`);
 
 })
